97cfd3e90db1da187ca87eb1cfca5b6eb9a13830
Archie Wang <64065330+Archie-King@users.noreply.github.com>
2025-01-09 17:42:38 +0800

    Insert user's info for test

--- Backend/src/main/resources/db/data.sql
+++ Backend/src/main/resources/db/data.sql
@@ -1,4 +1,8 @@
 insert into task (title, description, priority, due_date, completed)
-values
-('Task 1', 'Description for task 1', 1, '2025-01-10', false),
-('Task 2', 'Description for task 2', 2, '2025-01-10', false);
+values ('Task 1', 'Description for task 1', 1, '2025-01-10', false),
+       ('Task 2', 'Description for task 2', 2, '2025-01-10', false);
+
+insert into users (username, email, password, roles)
+values ('user', 'user@test.com','user', false),
+       ('admin', 'admin@admin.com', 'admin',true)
+ON CONFLICT (email) DO NOTHING;
